5363cbe3031d37d984d9edcfb8fb55436c545b12
khabib shahbanov <khabibtime@rambler.ru>
2026-03-06 01:46:38 +0300

    fixed

--- src/yatl/run.py
+++ src/yatl/run.py
@@ -3,19 +3,50 @@ from utils import search_files
 from step_executor import StepExecutor
 from extractor import DataExtractor
 from render import TemplateRenderer
+import yaml
 
 
 class Runner:
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        data_extractor: DataExtractor,
+        template_render: TemplateRenderer,
+    ):
+        self.data_extractor = data_extractor
+        self.template_render = template_render
+        self.step_executor = StepExecutor(data_extractor, template_render)
+
+    def create_context(self, test_spec: dict):
+        context = {}
+        stop = "steps"
+        for k, v in test_spec.items():
+            if k == stop:
+                return context
+            context[k] = v
+        return context
+
+    def run_test(self, yaml_path: str):
+        with open(yaml_path, "r", encoding="utf-8") as f:
+            test_spec: dict = yaml.safe_load(f)
+
+        context = self.create_context(test_spec)
+
+        print(f"Run test: {test_spec.get('name', '')}")
+        steps = test_spec.get("steps", [])
+        for i, step in enumerate(steps, start=1):
+            print(f"Step {i}: {step.get('name', '')}")
+            context = self.step_executor.run_step(step, context)
+
+        print("Test has been completed")
 
 
 if __name__ == "__main__":
     path = os.getcwd()
-    data_extractor = DataExtractor()
-    template_render = TemplateRenderer()
-    step_executor = StepExecutor(data_extractor, template_render)
+    run = Runner(
+        DataExtractor(),
+        TemplateRenderer(),
+    )
     print("-" * 10)
     for file in search_files(path, ".", []):
-        step_executor.run_test(file)
+        run.run_test(file)
         print("-" * 10)
--- src/yatl/step_executor.py
+++ src/yatl/step_executor.py
@@ -3,7 +3,6 @@ from extractor import DataExtractor
 from validator import ResponseValidator
 from request_builder import RequestBuilder
 import requests
-import yaml
 
 
 class StepExecutor:
@@ -28,26 +27,3 @@ class StepExecutor:
             context.update(extracted)
 
         return context
-
-    def create_context(self, test_spec: dict):
-        context = {}
-        stop = "steps"
-        for k, v in test_spec.items():
-            if k == stop:
-                return context
-            context[k] = v
-        return context
-
-    def run_test(self, yaml_path: str):
-        with open(yaml_path, "r", encoding="utf-8") as f:
-            test_spec: dict = yaml.safe_load(f)
-
-        context = self.create_context(test_spec)
-
-        print(f"Run test: {test_spec.get('name', '')}")
-        steps = test_spec.get("steps", [])
-        for i, step in enumerate(steps, start=1):
-            print(f"Step {i}: {step.get('name', '')}")
-            context = self.run_step(step, context)
-
-        print("Test has been completed")
